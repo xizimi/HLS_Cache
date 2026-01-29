34e77ae4d8da3dbb36a1e5f48ac6dcd11aa59b50
xizimi <2486637684@qq.com>
2026-01-29 22:34:29 +0800

    init

---
+++ geecache/geecache.go
@@ -0,0 +1,197 @@
+package geecache
+
+import (
+	"fmt"
+	pb "geecache/geecachepb"
+	"geecache/singleflight"
+	"log"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	defaultHotCacheRatio      = 8
+	defaultMaxMinuteRemoteQPS = 10
+)
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+	hotCache  cache
+	peers     PeerPicker
+	// use singleflight.Group tp make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
+	keys   map[string]*KeyStats
+}
+
+type KeyStats struct {
+	firstGetTime time.Time
+	remoteCnt    AtomicInt
+}
+
+type AtomicInt int64
+
+func (i *AtomicInt) Add(n int64) {
+	atomic.AddInt64((*int64)(i), n)
+}
+
+func (i *AtomicInt) Get() int64 {
+	return atomic.LoadInt64((*int64)(i))
+}
+
+// A Getter loads data for a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter witha function
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter inteface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup creates a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+		hotCache:  cache{cacheBytes: cacheBytes / defaultHotCacheRatio},
+		loader:    &singleflight.Group{},
+		keys:      make(map[string]*KeyStats),
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.hotCache.get(key); ok {
+		log.Println("[GeeCache] hot cache hit")
+		return v, nil
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] main cache hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	//通过阻塞防止缓存击穿
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
+			}
+		}
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
+	}
+	return
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
+	if err != nil {
+		return ByteView{}, err
+	}
+
+	value := ByteView{b: res.Value}
+
+	g.updateKeyStats(key, value)
+
+	return value, nil
+}
+
+func (g *Group) updateKeyStats(key string, value ByteView) {
+	// mu.Lock()
+	// defer mu.Unlock()
+	// 更新键的访问统计信息
+	if stat, ok := g.keys[key]; ok {
+		stat.remoteCnt.Add(1)
+		interval := float64(time.Now().Unix()-stat.firstGetTime.Unix()) / 60
+		qps := stat.remoteCnt.Get() / int64(math.Max(1, math.Round(interval)))
+		// 如果 QPS 超过阈值，将数据添加到热点缓存
+		if qps >= defaultMaxMinuteRemoteQPS {
+			g.populateHotCache(key, value)
+			mu.Lock()
+			delete(g.keys, key)
+			mu.Unlock()
+		}
+	} else {
+		// 首次访问，初始化统计信息
+		g.keys[key] = &KeyStats{
+			firstGetTime: time.Now(),
+			remoteCnt:    1,
+		}
+	}
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
+
+func (g *Group) populateHotCache(key string, value ByteView) {
+	g.hotCache.add(key, value)
+}
+
+// RegisterPeers registers a PeerPicker for choosing remote peer.
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"geecache"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func createGroup() *geecache.Group {
+	return geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+}
+
+func startCacheServer(addr string, addrs []string, gee *geecache.Group) {
+	peers := geecache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *geecache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+// startCacheServerGrpcEtcd 函数：
+// 创建一个 geecache.Server 实例，该实例用于处理 gRPC 请求并与其他节点通信。
+// 通过 geecache.Server 实例的 Set 方法设置一组节点地址。
+// 将 geecache.Server 实例注册到缓存组（gee）中。
+// 启动 geecache.Server 实例，开始处理 gRPC 请求。
+func startCacheServerGrpcEtcd(addr string, addrs []string, gee *geecache.Group) {
+	peers, _ := geecache.NewServer(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("geecache is running at ", addr)
+	err := peers.Start()
+	if err != nil {
+		peers.Stop()
+	}
+}
+
+/*
+用户通过 API 服务器（例如 http://localhost:9999）访问 /api?key=XXX 的形式来获取缓存数据。
+API 服务器会调用对应缓存组的 Get 方法。
+Get 方法首先尝试从本地缓存中的热点缓存（hotCache）中查找数据。
+如果数据不在热点缓存中，它将尝试从主缓存（mainCache）中查找数据。
+如果主缓存中也没有数据，缓存系统将选择一个远程节点（可能是本地节点，也可能是其他节点）。
+如果选中的远程节点是本地节点，缓存系统会直接从数据源获取数据。
+如果选中的远程节点不是本地节点，API 服务器将发送 gRPC 请求给对应的远程节点，要求其提供数据。
+
+注意：节点刚开始都会注册到etcd。
+*/
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "GeeCache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "127.0.0.1:8001",
+		8002: "127.0.0.1:8002",
+		8003: "127.0.0.1:8003",
+	} //grpc版本（含etcd）
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+	gee := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServerGrpcEtcd(addrMap[port], addrs, gee) //grpc版本
+}
